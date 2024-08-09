dbf9778525bd3ed61465ca19639e90f683dc8465
anhnh-n299 <anhnhn299@gmail.com>
2024-08-09 15:41:36 +0700

    Refactor

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abcd3eae571f84d7faa0db9b3bfaefc8-2094874635.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
